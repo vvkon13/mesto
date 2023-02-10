757ca2c784bc34665a42242b200508589a5fd1b5
Vasiliy Konovalov <vvkon13@gmail.com>
2023-02-11 01:46:52 +0400

    Montana

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ let popupName = document.querySelector('.popup__input_name');
 let popupDescription = document.querySelector('.popup__input_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
+let likesofpage = document.getElementsByClassName ('card__button-like');
 
 
 function callpopup() {
@@ -25,7 +26,16 @@ function savepopup(evt) {
   closepopup();
 }
 
+function clicklike() {
+  this.classList.toggle('card__button-like_active');
+}
+
 
 editbutton.addEventListener('click', callpopup);
 closebutton.addEventListener('click', closepopup);
 formChangeProfile.addEventListener('submit', savepopup);
+for (let i = 0; i < likesofpage.length; ++i) {
+  const button = likesofpage[i];
+  button.addEventListener('click', clicklike);
+}
+
