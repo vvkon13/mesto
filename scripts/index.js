fb28961b8a0e627e39058d600a04076a38737d6d
Vasiliy Konovalov <vvkon13@gmail.com>
2023-02-12 15:03:31 +0400

    Montana ver1.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,41 @@
 let editbutton = document.querySelector('.profile__button-edit');
 let closebutton = document.querySelector('.popup__button-close');
-let formChangeProfile = document.forms.PopupProfile;
+let formChangeProfile = document.forms['popup-profile'];
 let popupView = document.querySelector('.popup');
-let popupName = document.querySelector('.popup__input_name');
-let popupDescription = document.querySelector('.popup__input_description');
+let popupName = document.querySelector('.popup__input_type_name');
+let popupDescription = document.querySelector('.popup__input_type_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-let likesofpage = document.getElementsByClassName('card__button-like');
+// let likesofpage = document.getElementsByClassName('card__button-like');
 
 
-function callpopup() {
+function callPopup() {
   popupName.value = profileName.textContent;
   popupDescription.value = profileDescription.textContent;
   popupView.classList.add('popup_opened');
 }
 
-function closepopup() {
+function closePopup() {
   popupView.classList.remove('popup_opened');
 }
 
-function savepopup(evt) {
+function savePopup(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileDescription.textContent = popupDescription.value;
-  closepopup();
+  closePopup();
 }
 
-function clicklike() {
+/* function clickLike() {
   this.classList.toggle('card__button-like_active');
 }
+ */
 
-
-editbutton.addEventListener('click', callpopup);
-closebutton.addEventListener('click', closepopup);
-formChangeProfile.addEventListener('submit', savepopup);
-for (let i = 0; i < likesofpage.length; ++i) {
+editbutton.addEventListener('click', callPopup);
+closebutton.addEventListener('click', closePopup);
+formChangeProfile.addEventListener('submit', savePopup);
+/* for (let i = 0; i < likesofpage.length; ++i) {
   const button = likesofpage[i];
-  button.addEventListener('click', clicklike);
+  button.addEventListener('click', clickLike);
 }
-
+ */
